94a01ba25b8ed8728512ba02c21a0516354ebda0
heewon <choheewon@hanmail.net>
2025-09-09 14:59:14 +0900

    01-front-ci

---
+++ src/containers/LoginCon.js
@@ -0,0 +1,6 @@
+import LoginCom from "../components/LoginCom"
+import HeaderCom from "../components/common/HeaderCom"
+const LoginCon = () => {
+    return (<><HeaderCom /><LoginCom /></>)
+}
+export default LoginCon
